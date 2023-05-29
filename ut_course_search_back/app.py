bc132025840bafbe48298bcd407b2074d3c8e384
Afnan Mir <mirafnan12@gmail.com>
2023-05-28 17:12:00 -0700

    Change backend dockerfile

--- ut_course_search_back/app.py
+++ ut_course_search_back/app.py
@@ -11,8 +11,7 @@ import pinecone
 import db.index_methods as index_methods
 from responses.responses import Response
 
-app = Flask(__name__)
-CORS(app)
+
 
 index = None
 index_name = "ut-courses"
@@ -20,51 +19,96 @@ response = None
 
 
 
-@app.route("/")
-def home():
-    return "This is a test"
+# @app.route("/")
+# def home():
+#     return "This is a test"
 
-def get_course_and_description(query_response, top_k: int = 20, similarity_threshold: float = 0.70):
-    courses_and_descriptions = []
-    response = {}
-    matches = query_response['results'][0]['matches'][0:top_k]
-    for match in matches:
-        if(match['score'] > similarity_threshold):
-            course_title = match['metadata']['title']
-            course_description = match['metadata']['description']
-            course_and_description = {"title": course_title, "description": course_description}
-            courses_and_descriptions.append(course_and_description)
-    response["response"] = courses_and_descriptions
-    return response
+# def get_course_and_description(query_response, top_k: int = 20, similarity_threshold: float = 0.70):
+#     courses_and_descriptions = []
+#     response = {}
+#     matches = query_response['results'][0]['matches'][0:top_k]
+#     for match in matches:
+#         if(match['score'] > similarity_threshold):
+#             course_title = match['metadata']['title']
+#             course_description = match['metadata']['description']
+#             course_and_description = {"title": course_title, "description": course_description}
+#             courses_and_descriptions.append(course_and_description)
+#     response["response"] = courses_and_descriptions
+#     return response
 
-@app.route("/courses", methods=["GET"])
-def get_courses():
-    global index, response
-    query = request.args.get("text")
-    query_response = index_methods.query_index(index, query, top_k=20)
-    response.set_pinecone_response(query_response)
-    courses_and_descriptions = get_course_and_description(response.get_pinecone_response())
-    return courses_and_descriptions, 200
+# @app.route("/courses", methods=["GET"])
+# def get_courses():
+#     global index, response
+#     query = request.args.get("text")
+#     query_response = index_methods.query_index(index, query, top_k=20)
+#     response.set_pinecone_response(query_response)
+#     courses_and_descriptions = get_course_and_description(response.get_pinecone_response())
+#     return courses_and_descriptions, 200
 
-@app.route("/chat", methods=["GET"])
-def get_chat():
-    global index, response
-    query = request.args.get("text")
-    query_response = response.get_pinecone_response()
-    context = index_methods.extract_context(query_response)
-    chat_response = index_methods.query_chat(context, query)
-    response.set_openai_response(chat_response)
-    text = chat_response['choices'][0]['message']['content']
-    return {"text": text}, 200
+# @app.route("/chat", methods=["GET"])
+# def get_chat():
+#     global index, response
+#     query = request.args.get("text")
+#     query_response = response.get_pinecone_response()
+#     context = index_methods.extract_context(query_response)
+#     chat_response = index_methods.query_chat(context, query)
+#     response.set_openai_response(chat_response)
+#     text = chat_response['choices'][0]['message']['content']
+#     return {"text": text}, 200
 
 
 
 
 # if __name__ == "__main__":
-load_dotenv()
-print("Initializing index...")
-# initialize_index(get_course_urls(), os.getenv("INDEX_DIR"))
-index = index_methods.init_pinecone(index_name)
-response = Response()
-print("Index initialized!")
-app.run(host="0.0.0.0",debug=True)
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    index = None
+    index_name = "ut-courses"
+    response = None
+
+
+
+    @app.route("/")
+    def home():
+        return "This is a test"
+
+    def get_course_and_description(query_response, top_k: int = 20, similarity_threshold: float = 0.70):
+        courses_and_descriptions = []
+        response = {}
+        matches = query_response['results'][0]['matches'][0:top_k]
+        for match in matches:
+            if(match['score'] > similarity_threshold):
+                course_title = match['metadata']['title']
+                course_description = match['metadata']['description']
+                course_and_description = {"title": course_title, "description": course_description}
+                courses_and_descriptions.append(course_and_description)
+        response["response"] = courses_and_descriptions
+        return response
+
+    @app.route("/courses", methods=["GET"])
+    def get_courses():
+        global index, response
+        query = request.args.get("text")
+        query_response = index_methods.query_index(index, query, top_k=20)
+        response.set_pinecone_response(query_response)
+        courses_and_descriptions = get_course_and_description(response.get_pinecone_response())
+        return courses_and_descriptions, 200
+
+    @app.route("/chat", methods=["GET"])
+    def get_chat():
+        global index, response
+        query = request.args.get("text")
+        query_response = response.get_pinecone_response()
+        context = index_methods.extract_context(query_response)
+        chat_response = index_methods.query_chat(context, query)
+        response.set_openai_response(chat_response)
+        text = chat_response['choices'][0]['message']['content']
+        return {"text": text}, 200
+    load_dotenv()
+    print("Initializing index...")
+    # initialize_index(get_course_urls(), os.getenv("INDEX_DIR"))
+    index = index_methods.init_pinecone(index_name)
+    response = Response()
+    print("Index initialized!")
+    return app
